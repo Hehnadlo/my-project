4fb6f813f330322f817430d4fa7079410935e523
Hehnadlo <hehnadlo@gmail.com>
2025-10-14 20:17:31 +0200

    TEST pridania

---
+++ rand.c
@@ -0,0 +1,25 @@
+#include <stdio.h>                                                    
+#include <stdlib.h>  
+
+//funkcia generovania nahodneho cisla v rozsahu min, max
+int nahodneCislo(int min, int max) {
+        return (rand () % (max - min + 1)) + min;
+
+}
+
+int main(void) {
+
+    //pole s 10 timi prvkami
+    int array[10];
+ 
+    // naplnenie pola nahodnymi cislami
+    for (int i = 0; i < 10; i++) {
+        array[i] = nahodneCislo(0, 9);  // ulozenie cisla do pola
+    }
+
+    // vypisanie obshau pola
+    for (int i = 0; i < 10; i++) {
+        printf("array[%d] = %d\n", i, array[i]);
+    }
+    return 0;
+}
