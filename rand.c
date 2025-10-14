d3698c886e646304d1bbcb16e21c97bdc0a92365
Hehnadlo <hehnadlo@gmail.com>
2025-10-14 21:44:25 +0200

    Doplnenie zadania ulohy

--- rand.c
+++ rand.c
@@ -1,3 +1,10 @@
+/*
+Uloha c 1
+Vytvorte jednorozmerne pole s velkostou 10 a naplnte ho nahodnymi celymi cislami. Cisla generujte pomocou funkcie, ktora ako argumenty prijima minimalnu a maximalnu hodnotu. Po naplneni pola vypiste kazdy prvok spolu s jeho indexom pricom na vypis pouzite samostatnu funkciu.
+ 
+ Autor: Jana Chytilova
+ */
+
 #include <stdio.h>                                                    
 #include <stdlib.h>  
 
