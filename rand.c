d28651022735ca00b2c69386fbd434cda30a32d7
Hehnadlo <hehnadlo@gmail.com>
2025-10-29 14:11:19 +0100

    test

---
+++ rand.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  test4
+//
+
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void) {
+    
+    int i;
+    
+    for (i=1; i<=10; i++) {
+        printf("%0*d\n",6,i * i);
+        printf("%05o\n", i * i);
+    }
+    
+    printf("Slepice dělá %*.*s.\n", "koTkokodák \n");
+    return 0;
+}
